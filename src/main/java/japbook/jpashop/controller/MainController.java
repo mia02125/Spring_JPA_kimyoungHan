613aad851f92a4fe84260c4657136e39f321d47e
jaeminchoi <gis-law>
2022-09-02 19:14:33 +0900

    Controller 만들기 View 만들기 리빌딩 하는법 ( devtools 설치 )

---
+++ src/main/java/japbook/jpashop/controller/MainController.java
@@ -0,0 +1,17 @@
+package japbook.jpashop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+
+    @GetMapping("")
+    public String main(Model model) {
+        model.addAttribute("data", "hellow");
+        return "index";
+
+    }
+}
